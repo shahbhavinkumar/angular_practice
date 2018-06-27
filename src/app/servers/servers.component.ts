c61b7db74f1867855b53ec4fcdc2f7de519cd9d7
Bhavin Shah <bshah@beamsoftware.com>
2018-06-27 17:01:35 -0400

    2 Way binding + directives (not completed)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
+
+
 export class ServersComponent implements OnInit {
 
   allowNewServers = false;
   serverCreationStatus = 'No Servers Created';
-  serverName = '';
+  serverName = 'Apple Server';
+  serverCreate = false;
+
+  //data binding assignment
+  userName = 'Bhavin Shah';
 
   constructor() {
-    setTimeout(()=>{
-      this.allowNewServers=true;
-    },2000);
-   }
+    setTimeout(() => {
+      this.allowNewServers = true;
+    }, 2000);
+  }
+
+  onCreateServer(){
+    this.serverCreate = true;
+    this.serverCreationStatus = 'Server Created! Name is'+this.serverName;
+  }
+
 
   ngOnInit() {
   }
-
-  onCreateServer()
-  {
-    this.serverCreationStatus = 'Server was Created';
-  }
-
-  onUpdatedServerName(event:Event)
-  {
-   this.serverName = (<HTMLInputElement>event.target).value;
-  }
-
 }
